3513174a3e924359762ebe525fc744ae12e1079c
Vishal Kumar Gourav <vishal.vs911@gmail.com>
2016-06-26 14:36:30 +0530

    pandx.c completed

--- mathematical algo/pandx.c
+++ mathematical algo/pandx.c
@@ -3,7 +3,19 @@
 	PROBLEM	:	GIVEN n and p FIND LARGEST x SUCH THAT p^x DIVIDES n!
 */
 #include<stdio.h>
+int findMax(int n,int p){
+	int x=0;
+	while(n){
+		x+=(n/p);
+		n/=p;
+	}
+	return x;
+}
 int main(){
-	
+	int n,p;
+	n=10;
+	p=3;
+	printf("\nMaximum value of x=%d",findMax(n,p));
+	printf("\n");
 	return 0;
 }
